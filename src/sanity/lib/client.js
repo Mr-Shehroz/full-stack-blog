a74532d7bd6929ee1e19d1a620ed7621bc113241
Mr-Shehroz <mr.shehroz.khan2006>
2025-03-20 15:25:55 +0500

    add

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -3,9 +3,9 @@ import { createClient } from 'next-sanity'
 import { apiVersion} from '../env'
 
 const client = createClient({
-  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false,
 })
 export default client
